c6ae4a0ee9fb4d784f893404854d99caf72a347b
Niranjan <niranjankala@aol.com>
2018-01-24 06:08:14 -0800

    drag drop

---
+++ OSMViewer/Constants.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace OpenStudioMeasuresViewer
+{
+    public class Constants
+    {
+        public const string TextNoMeasuresSelected = "No measures selected";
+
+
+        public const string BAND_ZONE = "ZONE";
+        public const string BAND_GROUP = "GROUP";
+        public const string BAND_SIMULATION = "SIMULATION";
+        public const string COLUMN_CONFIGURATION = "Configuration";
+        public const string COLUMN_DATE = "Date";
+        public const string COLUMN_SIMULATION = "Simulation Parameters";
+        public const string COLUMN_SIMULATION_STATUS = "Simulation Status";
+        public const string COLUMN_REPORT = "Request Set";
+        public const string COLUMN_OUTPUT = "Output Format Set";
+        public const string COLUMN_View = "View";
+        public const string COLUMN_CHANGE_MEASURE_ORDER = "Change Measure Order";
+        public const string COLUMN_DELETE_ROW = "Delete Row";
+    }
+}
